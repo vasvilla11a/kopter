d804afdba763d3f48c8b3eecbf8861b9556373dc
M108-11 <M108-11@DESKTOP-KMCCGNH>
2023-03-02 09:20:44 +0100

    Add project files.

---
+++ kugli/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace kugli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
